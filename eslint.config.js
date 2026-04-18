79a8fc1d2c9bbe7d3f55b95322e9384c4066d611
hletassey-ui <hletassey@gmail.com>
2026-04-18 17:59:30 +0200

    Add ESLint configuration for JavaScript files

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ['src/**/*.js', 'tests/**/*.js'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+  },
+];
